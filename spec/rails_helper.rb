e4da9d078a1e48b1b203d07214931508e8b5652a
beatrizChagas <beatriznery.12@gmail.com>
2024-02-25 23:42:06 -0300

    feat: Setup database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'database_cleaner'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -62,4 +63,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Start by truncating all the tables but then use the faster transaction
+  # strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # Start the transaction strategy as examples are run
+  config.around(:each) { |example| DatabaseCleaner.cleaning { example.run } }
 end
