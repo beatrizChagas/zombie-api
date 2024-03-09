2583867a3d66ed76484c5b5a9f239e7ae8b65f3a
beatrizChagas <beatriznery.12@gmail.com>
2024-03-09 19:36:10 -0300

    test: Add test scenario for user update

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -21,11 +21,43 @@ RSpec.describe 'api/v1/users', type: :request do
 
       response '201', 'user created' do
         let(:user) { create(:user) }
+
         run_test!
       end
 
       response '422', 'invalid request' do
-        let(:user) {{ name: 'test-user', age: 30, gender: 'male' }}
+        let(:user) { { name: 'test-user', age: 30, gender: 'male' } }
+
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{id}' do
+    parameter name: 'id', in: :path, type: :integer, description: 'id'
+
+    put 'Updates a user location' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          latitude: { type: :number, format: :float },
+          longitude: { type: :number, format: :float }
+        }
+      }
+
+      response '204', 'user location updated' do
+        let(:id) { create(:user).id }
+        let(:user) { { latitude: 1234, longitude: 5678 } }
+
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:id) { -1 }
+        let(:user) { { latitude: 1234, longitude: 5678 } }
+
         run_test!
       end
     end
