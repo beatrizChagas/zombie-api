61d7dc1a4d646b91d6fbd8fcbc5708a61e8049af
beatrizChagas <beatriznery.12@gmail.com>
2024-03-09 00:11:02 -0300

    feat: Add User#create_inventory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_one :inventory
+  after_create :create_inventory
+
+  has_one :inventory, dependent: :destroy
 
   enum gender: { female: 0, male: 1 }
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :age, :gender, :latitude, :longitude, presence: true
+
+  private
+
+  def create_inventory
+    Inventory.create(user: self)
+  end
 end
