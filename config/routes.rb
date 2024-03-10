a21c1c722718fe31596e58af0481a85b0905a6b9
beatrizChagas <beatriznery.12@gmail.com>
2024-03-10 00:32:22 -0300

    feat: Add post `api_v1_user_infection` route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[create update]
+      resources :users, only: %i[create update] do
+        resource :infection, only: :create
+      end
     end
   end
 end
