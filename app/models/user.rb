c15e7e26501147acc3a0ce460c8f706cf8626b98
beatrizChagas <beatriznery.12@gmail.com>
2024-03-10 00:34:17 -0300

    feat: Add User#infected?

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   after_create :create_inventory
 
   has_one :inventory, dependent: :destroy
+  has_many :infection, dependent: :destroy
 
   enum gender: { female: 0, male: 1 }
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :age, :gender, :latitude, :longitude, presence: true
+
+  def infected?
+    Infection.where(infected_user: self).distinct.count >= 3
+  end
 
   private
 
