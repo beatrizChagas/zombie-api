f6afb8bdc154515a496077f9ac4a4e032b621635
beatrizChagas <beatriznery.12@gmail.com>
2024-03-12 13:02:23 -0300

    refactor: Add Infection.infected_user_ids

--- app/models/infection.rb
+++ app/models/infection.rb
@@ -5,28 +5,24 @@ class Infection < ApplicationRecord
   belongs_to :user, class_name: 'User'
 
   def self.infected_users_count
-    infected_users = group(:infected_user_id).distinct.count(:user_id)
-
-    infected_user_ids = infected_users.select { |infected_user_id, count| count >= 3 }.keys
-
     User.where(id: infected_user_ids).count
   end
 
   def self.non_infected_users_count
-    infected_users = group(:infected_user_id).distinct.count(:user_id)
-
-    infected_user_ids = infected_users.select { |infected_user_id, count| count >= 3 }.keys
-
     User.where.not(id: infected_user_ids).count
   end
 
   def self.lost_points
-    infected_users = group(:infected_user_id).distinct.count(:user_id)
-
-    infected_user_ids = infected_users.select { |infected_user_id, count| count >= 3 }.keys
-
     users = User.where(id: infected_user_ids)
 
     users.map { |user| user.inventory.total_points }.sum
+  end
+
+  private
+
+  def self.infected_user_ids
+    infected_users = group(:infected_user_id).distinct.count(:user_id)
+
+    infected_users.select { |infected_user_id, count| count >= 3 }.keys
   end
 end
