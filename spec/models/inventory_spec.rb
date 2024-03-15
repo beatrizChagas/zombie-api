96c369599dd73249d886c024d61fbdc12e4636af
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 11:17:15 -0300

    test: Add test scenarios for .calculate_points

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
@@ -7,5 +9,15 @@ RSpec.describe Inventory, type: :model do
 
   describe 'validations' do
     it { should validate_length_of(:items) }
+    it { should validate_presence_of(:user_id) }
+  end
+
+  describe '.calculate_points' do
+    it 'returns the points of the items' do
+      user = create(:user)
+      create(:inventory, user:)
+
+      expect(Inventory.calculate_points('water', 1)).to eq(4)
+    end
   end
 end
