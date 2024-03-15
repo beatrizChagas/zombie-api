6863631e830965c1e8bc8f4f4eadfde37bbb4114
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 09:48:45 -0300

    feat: Include JsonResponse in UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,15 +3,17 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      include JsonResponse
+
       # POST /api/v1/users
       # Creates a new user
       def create
         @user = User.new(user_params)
 
         if @user.save
-          render json: UserBlueprint.render(@user), status: :created
+          render_blueprint(UserBlueprint, @user, :created)
         else
-          render @user.errors, status: :unprocessable_entity
+          render_error(@user.errors, :unprocessable_entity)
         end
       end
 
