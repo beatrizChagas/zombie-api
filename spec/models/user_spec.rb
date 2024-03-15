1cbfb1bc64cb9d864fe4c35d7faf0d2e1756e036
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 10:15:40 -0300

    test: Add test scenario to #infected?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:latitude) }
     it { should validate_presence_of(:longitude) }
   end
+
+  describe '#infected?' do
+    it 'returns true when user is infected' do
+      user = create(:user)
+      user2 = create(:user)
+      user3 = create(:user)
+      user4 = create(:user)
+
+      create(:infection, infected_user: user, user: user2)
+      create(:infection, infected_user: user, user: user3)
+      create(:infection, infected_user: user, user: user4)
+
+      expect(user.infected?).to be true
+    end
+
+    it 'returns false when user is not infected' do
+      user = create(:user)
+
+      expect(user.infected?).to be false
+    end
+  end
 end
