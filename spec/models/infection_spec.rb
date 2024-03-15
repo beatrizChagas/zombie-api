6005b496d5e57af78f705e68ad248de43e5524d7
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 10:38:31 -0300

    test: Add test scenario for `.non_infected_users_count`

--- spec/models/infection_spec.rb
+++ spec/models/infection_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Infection, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:infected_user) }
   end
+
+  describe '.infected_users_count' do
+    it 'returns the number of infected users' do
+      user = create(:user)
+      user2 = create(:user)
+      user3 = create(:user)
+      infected_user = create(:user)
+
+      create(:infection, user: user, infected_user: infected_user)
+      create(:infection, user: user2, infected_user: infected_user)
+      create(:infection, user: user3, infected_user: infected_user)
+
+      expect(Infection.infected_users_count).to eq(1)
+    end
+  end
 end
