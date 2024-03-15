7655f1716af0293fb2cd7dfbf2835e8e048b5ed2
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 09:51:03 -0300

    feat: Include JsonResponse in InfectionsController

--- app/controllers/api/v1/infections_controller.rb
+++ app/controllers/api/v1/infections_controller.rb
@@ -3,15 +3,17 @@
 module Api
   module V1
     class InfectionsController < ApplicationController
+      include JsonResponse
+
       # POST /api/v1/users/:user_id/infection
       # Reports an infected user
       def create
         @infection = Infection.new(infection_params)
 
         if @infection.save
-          render json: InfectionBlueprint.render(@infection), status: :created
+          render_blueprint(InfectionBlueprint, @infection, :created)
         else
-          render @infection.errors, status: :unprocessable_entity
+          render_error(@infection.errors, :unprocessable_entity)
         end
       end
 
