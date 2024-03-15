b961bf68a3840e8cf82e0866a0a8b17b27eaa070
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 10:12:45 -0300

    test: Add have_many association spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_one(:inventory) }
+    it { should have_many(:infection) }
   end
 
   describe 'validations' do
