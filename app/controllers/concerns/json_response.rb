21fa9a1580f542fd20f71b20c6ddd17f80c925ff
beatrizChagas <beatriznery.12@gmail.com>
2024-03-15 16:12:53 -0300

    feat: Add #render_percentage and #render_custom_json to JsonResponse

--- app/controllers/concerns/json_response.rb
+++ app/controllers/concerns/json_response.rb
@@ -12,4 +12,12 @@ module JsonResponse
   def render_error(message, status)
     render json: { error: message }, status: status
   end
+
+  def render_percentage(percentage, status)
+    render json: { Percentage: percentage }, status: status
+  end
+
+  def render_custom_json(data, status)
+    render json: data, status: status
+  end
 end
